49d6944f508a8bb15d11cad47fab549e18a8d208
Odhran <mahonyodhran@gmail.com>
2022-09-30 20:41:25 +0100

    working parsing title, price, url from particular query

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const express = require('express')
 
 const app = express()
 
-const url = 'https://www.thejournal.ie/'
+const url = 'https://www.tesco.ie/groceries/en-IE/search?query=chicken'
 
 axios(url)
     .then(response => {
         const html = response.data
-        console.log(html)
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('.product-list--list-item',  html).each(function(){
+            const title =  $(this).text()
+            const titleProper = $(this).find('span').text()
+            const url = 'https://www.tesco.ie/' + $(this).find('a').attr('href')
+            articles.push({
+                titleProper,
+                url
+            })
+        })
+        console.log(articles)
+
     }).catch(err => console.log(err))
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
